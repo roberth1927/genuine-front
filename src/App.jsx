1d99068a8ad3b8f59ae7cc776e54502801a4e5e2
Roberth <robinmoralesquiroz@gmail.com>
2024-06-19 11:31:30 -0500

    add initial files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <p className="read-the-docs">
+          Hola
+        </p>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
