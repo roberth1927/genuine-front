6e622976314cfa7dae49b33a135e3668785196b9
Roberth <robinmoralesquiroz@gmail.com>
2024-06-19 11:43:30 -0500

    Add tailwind to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import './App.css'
-
 function App() {
   
   return (
     <>
       <div>
-        <p className="read-the-docs">
-          Hola
-        </p>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
         
       </div>
     </>
