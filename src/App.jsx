73a10cc8418373ff9d052c367635daadc8a7d850
Roberth <robinmoralesquiroz@gmail.com>
2024-06-19 23:57:34 -0500

    pagination component creation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import Home from "./sections/Home";
 import About from "./sections/About";
 import Nav from "../src/components/home/Nav";
+import Pagination from "../src/components/home/Pagination";
 
 function App() {
   const homeRef = useRef(null);
@@ -21,6 +22,9 @@ function App() {
       </div>
       <div className="fixed hidden md:block top-0 right-0">
         <Nav handleScrollTo={handleScrollTo} refs={{ home: homeRef, about: aboutRef }} />
+      </div>
+      <div className="fixed hidden md:block bottom-2 right-0">
+        <Pagination />
       </div>
     </div>
   );
---
+++ src/components/home/Pagination.jsx
@@ -0,0 +1,16 @@
+import img1 from "../../assets/pagination.png";
+
+function Pagination() {
+  return (
+    <div className="relative w-[125px] h-[40px]">
+      <img
+        src={img1}
+        alt="Description"
+        className="absolute w-full h-full"
+      />
+    
+    </div>
+  );
+}
+
+export default Pagination;
